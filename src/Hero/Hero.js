7f41eae0194adda1d98cbc75b4e82e563b477003
ekansh <ekanshsin232@gmail.com>
2023-09-30 15:35:01 +0530

    carousel added

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -2,47 +2,99 @@ import { useEffect, useState } from "react";
 import "./Hero.css";
 
 const Hero = () => {
-  const [imageId, setImageId] = useState(0);
+  // const [imageId, setImageId] = useState(0);
+  const [showButton, setShowButton] = useState(false);
 
-  const imagesArray = [
-    "https://tecdn.b-cdn.net/img/new/slides/017.webp",
-    "https://tecdn.b-cdn.net/img/new/slides/010.webp",
-    "https://tecdn.b-cdn.net/img/new/slides/011.webp",
-    "https://tecdn.b-cdn.net/img/new/slides/024.webp",
+
+  const mouseEnterHandler = () => {
+    setShowButton(true)
+  };
+
+  const mouseLeaveHandler = () => {
+    setShowButton(false)
+  };
+
+  let slides = [
+    "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
+    "https://wallpapercave.com/wp/wp3386769.jpg",
+    "https://wallpaperaccess.com/full/809523.jpg",
+    "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
   ];
+
+  let [current, setCurrent] = useState(0);
+
+  let previousSlide = () => {
+    if (current === 0) setCurrent(slides.length - 1);
+    else setCurrent(current - 1);
+  };
+
+  let nextSlide = () => {
+    if (current === slides.length - 1) setCurrent(0);
+    else setCurrent(current + 1);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setImageId((prev) => {
-        if (prev < imagesArray.length - 1) {
-          return prev + 1;
-        }
-        return 0;
-      });
+      // setImageId((prev) => {
+      nextSlide()
+      // });
     }, 6000);
     return () => {
       clearInterval(interval);
     };
-  }, [imagesArray.length]);
-
-  const mouseEnterHandler = () => {};
-
-  const mouseLeaveHandler = () => {};
+  }, [current]);
 
   return (
-    <div
-      //   data-aos="zoom-in"
-      //   data-aos-duration="1000"
-      className={`block rounded-lg w-[95%]  h-[80vh] ml-8   transition-transform duration-500 ease-out motion-reduce:transition-none bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 `}
+    <div className="w-full h-[70vh]  m-auto "
+
       onMouseEnter={mouseEnterHandler}
-      onMouseLeave={mouseLeaveHandler}
-    >
-      <img
-        className={`rounded-lg w-full h-full object-cover`}
-        src={imagesArray[imageId]}
-        alt=""
-      />
-    </div>
+      onMouseLeave={mouseLeaveHandler}>
+      <div className="overflow-hidden relative h-full">
+        <div
+          className={`flex transition ease-out duration-500`}
+          style={{
+            transform: `translateX(-${current * 100}%)`,
+          }}
+        >
+          {slides.map((s) => {
+            return <img src={s} />;
+          })}
+        </div>
+        {showButton &&
+          <div className="absolute top-0 h-full w-full justify-between items-center flex text-white px-10 text-3xl">
+            <button className="rounded-full p-2 " style={{ color: 'black', background: 'white' }} onClick={previousSlide}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+              </svg>
+
+            </button>
+            <button className="rounded-full p-2 " style={{ color: 'black', background: 'white' }} onClick={nextSlide}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+              </svg>
+
+            </button>
+          </div>}
+
+        {showButton && <div className="absolute bottom-0 py-4 flex justify-center items-center gap-3 w-full">
+          {slides.map((s, i) => {
+            return (
+              <div
+                onClick={() => {
+                  setCurrent(i);
+                }}
+                key={"circle" + i}
+                className={`rounded-full cursor-pointer ${i == current ? 'w-5  h-5' : 'w-2 h-2'}`}
+                style={{
+                  background: i == current ? "white" : "gray"
+                }}
+              ></div>
+            );
+          })}
+        </div>}
+      </div>
+    </div >
+
   );
 };
 
