7195d342059da3a7effb530eb894c1492e0a9685
GurushanthGowdar <guru22ca@gmail.com>
2023-10-01 14:14:56 +0530

    added the logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+import logo from "../../Image/Logo/Velu Mobile & Watches png 1.png";
+
 const Header = () => {
   return (
-    <div className="h-14 bg-header-main flex justify-between px-10 items-center sticky top-0">
-      <div>LOGO</div>
-      <div className="flex space-x-4">
+    <div className="h-14 bg-header-main flex justify-between px-10 items-center sticky z-50 top-0">
+      <div className="mt-4 mb-4">
+        <img src={logo}></img>
+      </div>
+      <div className="flex space-x-4 gap-5">
         <div className="flex-1">Home</div>
         <div className="flex-1">About</div>
         <div className="flex-1">Services</div>
